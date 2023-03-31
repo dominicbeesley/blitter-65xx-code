b5891a8804107c4d8c471793c949411f7cb83762
dominicbeesley <dominic@authorityfile.co.uk>
2023-03-31 14:14:45 +0100

    include clib rom, tilecutter fixes

--- src/demos/adventure/readme.md
+++ src/demos/adventure/readme.md
@@ -10,3 +10,11 @@ game editor
 
 Fast sprite plotting and preservation tech demo, use < and > keys for 
 more/fewer little Ishbels
+
+
+# Build notes
+
+This project requires Python 3 (with Pillow) and Perl (with XML::LibXML) it also requires the "BBC Micro" capable version of cc65 from https://github.com/dominicbeesley/cc65
+
+This project also uses the experimental clib rom (included) from https://github.com/dominicbeesley/cc65-clib
+
--- src/demos/adventure/scripts/tilecutter.py
+++ src/demos/adventure/scripts/tilecutter.py
@@ -49,290 +49,214 @@ Slice a bitmap up into game tiles""", file=fh)
     if exit != 0:
         sys.exit(exit)
 
+def main():
+    if len(sys.argv) != 3:
+        Usage(sys.stdout, "Incorrect number of arguments", exit=1)
 
-if len(sys.argv) != 3:
-    Usage(sys.stdout, "Incorrect number of arguments", exit=1)
+    basefile = sys.argv[1]
+    basedir = os.path.dirname(basefile)
+    sourcedir = sys.argv[2]
 
-basefile = sys.argv[1]
-basedir = os.path.dirname(basefile)
-sourcedir = sys.argv[2]
-
-os.path.isdir(sourcedir) or Usage(sys.stderr, f"Source directory \"{sourcedir}\" does not exist", exit=1)
-
-try:
-    d = ET.parse(basefile)
-except Exception as ex:
-    Usage(message=f"Error opening file {basefile}", ex=ex, exit=2)
-
-
-pal = []
-for i in range(16):    
-    if i == 8:
-        r = g = b = 128
-    else:
-        lvl = 127 if i <= 7 else 255
-        r = lvl if i & 1 else 0
-        g = lvl if i & 2 else 0
-        b = lvl if i & 4 else 0
-    pal.append((r, g, b))
-
-
-bm = None
-
-xPal = d.find("palette")
-
-if xPal is not None:
-    i = 0
-    for xpe in xPal.findall("ent"):
-        if i >= 16:
-            Usage(sys.stderr, "Too many palette entries", exit=2)
-        pal[i] = ImageColor.getrgb(xpe.text)
-        i+=1
-
-    if i < 16:
-        Usage(sys.stderr, "Too few palette entries", exit=2)
-
-    palFile = os.path.join(basedir,xPal.attrib["file"])
+    os.path.isdir(sourcedir) or Usage(sys.stderr, f"Source directory \"{sourcedir}\" does not exist", exit=1)
 
     try:
-        palFH=open(palFile, "wb")
+        d = ET.parse(basefile)
     except Exception as ex:
-        Usage(message=f"Error opening palette file \"{palFile}\" for output", ex=ex, exit=2)
-
-    with palFH:
-        for i in range(16):
-            palFH.write(bytes([(i << 4) + (pal[i][0] >> 4), (pal[i][1] & 0xF0) | (pal[i][2]>>4)]))
+        Usage(message=f"Error opening file {basefile}", ex=ex, exit=2)
 
 
-for xDf in d.findall("dest"):
-    destFileName = xDf.attrib["file"]
-
-    szX = int(xDf.attrib.get("size-x") or "16")
-    szY = int(xDf.attrib.get("size-y") or "16")
-    mask = (xDf.attrib.get("mask") or "").lower().startswith("y");
-    imgno = 0;
-    destFullName = os.path.join(basedir, destFileName)
-
-    cuts = ET.Element("cuts")
-
-    cuts.set("size-x", str(szX))
-    cuts.set("size-y", str(szY))
-    cuts.set("mask", "y" if mask else "n")
-
-    try:
-        destFH = open(destFullName, "wb")
-    except Exception as ex:
-        Usage(sys.stderr, f"Cannot open \"{destFullName}\" for output", ex=ex, exit=2)
-
-    bm=None
-    bmpData=None
-    bmpWidth=-1
-    bmpHeight=-1
-    curX=0
-    curY=0
-    tileIx=0
-    try:
-        for xe in xDf.findall("*"):
-            print(xe.tag)
-            if xe.tag == "source":
-                sourceFileName = xe.attrib["file"]
-                sourceFullName = os.path.join(sourcedir, sourceFileName);
-
-                if bm is not None:
-                    bm.close
-                    bm=None
-
-                try:
-                    bm = Image.open(sourceFullName)
-                except Exception as ex:
-                    Usage(sys.stderr, f"Cannot open source image file \"{sourceFullName}\" for input", ex=ex, exit=2)
-
-                bmpWidth = bm.width;
-                bmpHeight = bm.height;
-#                                         stride = 4 * ((width * 4 + 31) / 32);
-
-#                                         bytes = new byte[height, stride];
-
-#                                         destXw.WriteStartElement("source");
-#                                         destXw.WriteAttributeString("size-x", width.ToString());
-#                                         destXw.WriteAttributeString("size-y", height.ToString());
-#                                         destXw.WriteAttributeString("filename", sourceFileName);
+    pal = []
+    for i in range(16):    
+        if i == 8:
+            r = g = b = 128
+        else:
+            lvl = 127 if i <= 7 else 255
+            r = lvl if i & 1 else 0
+            g = lvl if i & 2 else 0
+            b = lvl if i & 4 else 0
+        pal.append((r, g, b))
 
 
+    bm = None
 
-    finally:
-        if bm is not None:
-            bm.close
-    cuts_doc = ET.ElementTree(cuts)
-    cuts_doc.write(destFullName, encoding="UTF-8", xml_declaration=True)
+    xPal = d.find("palette")
 
+    if xPal is not None:
+        i = 0
+        for xpe in xPal.findall("ent"):
+            if i >= 16:
+                Usage(sys.stderr, "Too many palette entries", exit=2)
+            pal[i] = ImageColor.getrgb(xpe.text)
+            i+=1
+
+        if i < 16:
+            Usage(sys.stderr, "Too few palette entries", exit=2)
+
+        palFile = os.path.join(basedir,xPal.attrib["file"])
+
+        try:
+            palFH=open(palFile, "wb")
+        except Exception as ex:
+            Usage(message=f"Error opening palette file \"{palFile}\" for output", ex=ex, exit=2)
+
+        with palFH:
+            for i in range(16):
+                palFH.write(bytes([(i << 4) + (pal[i][0] >> 4), (pal[i][1] & 0xF0) | (pal[i][2]>>4)]))
+
+
+    for xDf in d.findall("dest"):
+        destFileName = xDf.attrib["file"]
+
+        szX = int(xDf.attrib.get("size-x") or "16")
+        szY = int(xDf.attrib.get("size-y") or "16")
+        mask = (xDf.attrib.get("mask") or "").lower().startswith("y");
+        imgno = 0;
+        destFullName = os.path.join(basedir, destFileName)
+
+        cuts = ET.Element("cuts")
+
+        cuts.set("size-x", str(szX))
+        cuts.set("size-y", str(szY))
+        cuts.set("mask", "y" if mask else "n")
+
+        try:
+            destFH = open(destFullName, "wb")
+        except Exception as ex:
+            Usage(sys.stderr, f"Cannot open \"{destFullName}\" for output", ex=ex, exit=2)
+
+        bm=None
+        bmpData=None
+        bmpWidth=-1
+        bmpHeight=-1
+        curX=0
+        curY=0
+        tileIx=0
+        try:
+            for xe in xDf.findall("*"):
+                if xe.tag == "source":
+                    sourceFileName = xe.attrib["file"]
+                    sourceFullName = os.path.join(sourcedir, sourceFileName);
+
+                    print(f"SOURCE:{sourceFileName}")
+
+                    if bm is not None:
+                        bm.close
+                        bm=None
+
+                    try:
+                        bm = Image.open(sourceFullName)
+                    except Exception as ex:
+                        Usage(sys.stderr, f"Cannot open source image file \"{sourceFullName}\" for input", ex=ex, exit=2)
+
+                    bmpWidth = bm.width;
+                    bmpHeight = bm.height;
+                    pixels = bm.load()
+
+                    xdSource = ET.SubElement(cuts, "source")
+                    xdSource.set("size-x", str(bmpWidth));
+                    xdSource.set("size-y", str(bmpHeight));
+                    xdSource.set("filename", sourceFileName);
+
+                    # pixels16 contains palette indexes
+                    pixels16 = bmpTo16(pixels, bmpWidth, bmpHeight, pal, mask)
+
+                elif xe.tag == "move":
+                    curX = int(xe.attrib["x"])
+                    curY = int(xe.attrib["y"])
+                elif xe.tag == "cut":
+                    pixels is not None or Usage(sys.stderr, "Error: cut before source", exit=4)
+
+                    nX = int(xe.attrib["x"])
+                    nY = int(xe.attrib["y"])
+                    cutDir = xe.attrib["dir"]
+
+                    cutDir == "rd" or Usage(sys.stderr, f"Bad direction \"{cutDir}\" only (rd) supported")
+                    
+                    for j in range(nY):
+                        for i in range(nX):
+                            tileIx = DoCut(destFH, pixels16, bmpWidth, bmpHeight, szX, szY, curX + szX * i, curY + szY * j, mask, tileIx)
+
+        finally:
+            if bm is not None:
+                bm.close
+        cuts_doc = ET.ElementTree(cuts)
+        cuts_doc.write(destFullName + ".cuts", encoding="UTF-8", xml_declaration=True)
+
+def bmpTo16(pixels, width, height, pal, mask):
+    """Make a 4bpp bitmap, 0x80 for transparent bits"""    
+    pr = []
+    for y in range(height):
+        prr = [0] * width
+        for x in range(width):
+            c = pixels[x,y]
+            ix = nearestInPal(pal, c, mask)
+            prr[x] = ix
+        pr.append(prr)
+    return pr
+
+
+def nearestInPal(pal, c, mask):
+    """find nearest colour"""
+    if mask and c[3] < 128:
+        return 0x80
+    ix = 0
+    min = -1;
+    for i in range(16):
+        pr, pg, pb = toRGB(pal[i])
+        r, g, b = toRGB(c)
+        err = sq(r - pr) + sq(g - pg) + sq(b - pb)
+
+        if min < 0 or err < min:
+            min = err
+            ix = i
+    return ix
         
+def sq(x):
+    return x*x
+
+def toRGB(c):
+    return (c[0],c[1],c[2])    
+
+def DoCut(destFh, bmp16, bmpW, bmpH, szX, szY, leftX, topY, mask, tileIx):
+
+    print(f"CUT:{szX}, {szY}, {leftX}, {topY}, {mask}, {tileIx}")
 
 
-#                                         //make new 16 colour bitmap with our own palette
+    (leftX < 0 or topY < 0) and Usage(sys.stderr, f"Out of bounds cut at {leftX},{topY}")
+    (leftX + szX < 0 or topY + szY < 0) and Usage(sys.stderr, f"Out of bounds cut at {leftX + szX},{topY + szY}")
 
-#                                         width = (int)bm.Width;
-#                                         height = (int)bm.Height;
-#                                         stride = 4 * ((width * 4 + 31) / 32);
+    for j in range(szY):
+        srcY = topY + j
+        
+        beebPxRow = [0] * int((szX + 1) / 2)
 
-#                                         bytes = new byte[height, stride];
+        for i in range(szX):
+            srcX = leftX + i
+            ix = bmp16[srcY][srcX] & 0xF
 
-#                                         destXw.WriteStartElement("source");
-#                                         destXw.WriteAttributeString("size-x", width.ToString());
-#                                         destXw.WriteAttributeString("size-y", height.ToString());
-#                                         destXw.WriteAttributeString("filename", sourceFileName);
+            #print(f"RRRRP{bmp16[srcY]}")
 
-#                                         /*
-#                                         GCHandle gch = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-#                                         try
-#                                         {
-#                                             Bitmap bm16 = new Bitmap(bm.Width, bm.Height, stride, PixelFormat.Format4bppIndexed, gch.AddrOfPinnedObject());
+            if ((i & 1) == 0):
+                beebPxRow[i >> 1] |= BeebPixLeft(ix)
+            else:
+                beebPxRow[i >> 1] |= BeebPixRight(ix)
 
-#                                             ColorPalette p = bm16.Palette;
-
-#                                             for (int i = 0; i < 16; i++)
-#                                                 p.Entries[i] = pal[i];
-
-#                                             bm16.Palette = p;
-
-#                                             for (int x = 0; x < bm.Width; x++)
-#                                             {
-#                                                 for (int y = 0; y < bm.Height; y++)
-#                                                 {
-#                                                     Color oc = bm.GetPixel(x, y);
-#                                                     int ix = 0;
-#                                                     double max = 9999999;
-#                                                     //find nearest colour
-#                                                     for (int i = 0; i < 16; i++)
-#                                                     {
-#                                                         Color pc = pal[i];
-#                                                         double err = Math.Pow(oc.R - pc.R, 2) + Math.Pow(oc.G - pc.G, 2) + Math.Pow(oc.B - pc.B, 2);
-
-#                                                         if (err < max)
-#                                                         {
-#                                                             max = err;
-#                                                             ix = i;
-#                                                         }
-#                                                     }
-#                                                     bytes[y, x >> 1] |= (byte)(((x & 1) != 0) ? ix : ix << 4);
-#                                                 }
-#                                             }
-
-#                                             bm16.Save($"d:\\temp\\image-{imgno++}.bmp", ImageFormat.Bmp);
-
-#                                         }
-#                                         finally
-#                                         {
-#                                             gch.Free();
-#                                         }
-#                                         */
-
-#                                     }
-#                                     else if (xe.LocalName == "move")
-#                                     {
-#                                         curX = int.Parse(xe.GetAttribute("x"));
-#                                         curY = int.Parse(xe.GetAttribute("y"));
-#                                     }
-#                                     else if (xe.LocalName == "cut")
-#                                     {
-#                                         if (bytes == null)
-#                                         {
-#                                             Usage(Console.Error, "Error: cur before source");
-#                                             return 4;
-#                                         }
-
-#                                         int nX = int.Parse(xe.GetAttribute("x"));
-#                                         int nY = int.Parse(xe.GetAttribute("y"));
-#                                         string dir = xe.GetAttribute("dir");
-
-#                                         switch (dir)
-#                                         {
-#                                             case "rd":
-#                                                 for (int j = 0; j < nY; j++)
-#                                                     for (int i = 0; i < nX; i++)
-#                                                         DoCut(destB, bm, bytes, width, height, stride, szX, szY, curX + szX * i, curY + szY * j, mask, destXw, ref tileIx);
-#                                                 break;
-#                                             default:
-#                                                 Usage(Console.Error, $"Bad direction for cut=\"{dir}\"");
-#                                                 break;
-#                                         }
+        destFh.write(bytes(beebPxRow))
 
 
-#                                     }
-#                                 }
+    if mask:
+        for j in range(szY):
+            beebMaskRow = [0] * int((szX + 7) / 8)
+            srcY = topY + j;
+            for i in range(szX):
+                srcX = leftX + i;
+                if (bmp16[srcY][srcX] & 0x80) == 0:
+                    beebMaskRow[i >> 3] |= 1 << (7 - (i & 7));
 
-#                                 if (bytes != null)
-#                                     destXw.WriteEndElement();
-#                                 destXw.WriteEndElement();
+            print(beebMaskRow)
+            destFh.write(bytes(beebMaskRow))
 
-#                             }
-#                         }
-#                     }
-#                 }
-#             } finally
-#             {
-#                 if (bm != null)
-#                     bm.Dispose();
-#             }
-
-#             return 0;
-#         }
-
-#         protected void DoCut(BinaryWriter bw, IImage orgBm, byte[,] bmp, int bmpW, int bmpH, int bmpStride, int szX, int szY, int leftX, int topY, bool mask, XmlWriter xw, ref int tileIx)
-#         {
-#             if (bw == null)
-#                 throw new ArgumentNullException("bw");
-
-#             byte[] beebPxRow = new byte[((szX + 1) / 2)];
-
-#             if (bmp == null)
-#                 Usage(Console.Error, "No source set before a cut!");
-
-#             if (leftX < 0 || topY < 0)
-#                 Usage(Console.Error, $"Out of bounds cut at {leftX},{topY}");
-#             if (leftX + szX < 0 || topY + szY < 0)
-#                 Usage(Console.Error, $"Out of bounds cut at {leftX + szX},{topY + szY}");
-
-#             for (int j = 0; j < szY; j++)
-#             {
-#                 for (int i = 0; i < beebPxRow.Length; i++)
-#                     beebPxRow[i] = 0;
-
-#                 int srcY = topY + j;
-#                 for (int i = 0; i < szX; i++)
-#                 {
-#                     int srcX = leftX + i;
-#                     int ix = GetPixel(bmp, srcX, srcY);
-
-#                     if ((i & 1) == 0)
-#                         beebPxRow[i >> 1] |= BeebPixLeft(ix);
-#                     else
-#                         beebPxRow[i >> 1] |= BeebPixRight(ix);
-#                 }
-#                 bw.Write(beebPxRow);
-#             }
-
-#             if (mask)
-#             {
-#                 byte[] beebMaskRow = new byte[(szX + 7) / 8];
-
-#                 for (int j = 0; j < szY; j++)
-#                 {
-#                     for (int i = 0; i < beebMaskRow.Length; i++)
-#                         beebMaskRow[i] = 0;
-
-#                     int srcY = topY + j;
-#                     for (int i = 0; i < szX; i++)
-#                     {
-#                         int srcX = leftX + i;
-#                         int A = orgBm.GetPixel(srcX, srcY).A;
-
-#                         if (A > 128)
-#                             beebMaskRow[i >> 3] |= (byte)(1 << (7 - (i & 7)));
-#                     }
-#                     bw.Write(beebMaskRow);
-#                 }
-
+    return tileIx+1
 #             }
 
 #             xw.WriteStartElement("cut");
@@ -342,18 +266,14 @@ for xDf in d.findall("dest"):
 #             xw.WriteEndElement();
 #         }
 
-#         protected byte BeebPixLeft(int ix)
-#         {
-#             return (byte)(((ix & 8) << 3)
-#                 + ((ix & 4) << 2)
-#                 + ((ix & 2) << 1)
-#                 + (ix & 1));
-#         }
+def BeebPixLeft(ix):
+    return        (((ix & 8) << 3)
+                 + ((ix & 4) << 2)
+                 + ((ix & 2) << 1)
+                 + (ix & 1))
 
-#         protected byte BeebPixRight(int ix)
-#         {
-#             return (byte)(BeebPixLeft(ix) << 1);
-#         }
+def BeebPixRight(ix):
+    return BeebPixLeft(ix) << 1
 
 #         protected int GetPixel(byte[,] bmp, int x, int y)
 #         {
@@ -401,3 +321,6 @@ for xDf in d.findall("dest"):
 #         #endregion
 #     }
 # }
+
+
+main()
