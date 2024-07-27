a1812172aee9cecc360a87ef0ea20375346a4d16
dominicbeesley <dominic@authorityfile.co.uk>
2024-07-27 08:46:04 +0100

    NEW: more blitter examples

--- scripts/beebspriter2blit.pl
+++ scripts/beebspriter2blit.pl
@@ -3,37 +3,132 @@
 use strict;
 use Getopt::Long;
 use XML::LibXML;
+use MIME::Base64;
 
 # convert beebspriter XML to binary in Blitter linear form
 
 my $mask;
 my $symbols;
+my $help;
 GetOptions(
 	"mask" => \$mask,
-	"symbols" => \$symbols
+	"symbols" => \$symbols,
+	"help" => \$help
 	) or Usage("Bad options $!");
+
+if ($help) {
+	Usage(*STDOUT, undef, 0);
+}
 
 my $fn_in = shift;
 my $fn_out = shift;
-my %fn_syms = ($symbols)?shift:undef;
+my $fn_syms = ($symbols)?shift:undef;
 
-($fn_in and -e $fn_in) or Usage "Missing input filename";
-$fn_out or Usage "Missing output filename";
-$fn_symbols or !$symbols or die "Missing symbols filename";
+($fn_in and -e $fn_in) or Usage(*STDERR, "Missing input filename", 10);
+$fn_out or Usage(*STDERR, "Missing output filename", 10);
+$fn_syms or !$symbols or Usage(*STDERR, "Missing symbols filename", 10);
 
-my $dom = XML::LibXML->load_xml(location => $fn_in);
+my $dom = XML::LibXML->load_xml(location => $fn_in) or die "Cannot open $fn_in : $!";
+
+open(my $fh_out, ">:raw:", $fn_out) or die "Cannot open \"$fn_out\" for output : $!";
+
+$dom->documentElement->nodeName eq "SpriteSheet" or die "That doesn't look like a SpriteSheet";
+
+my $mode = $dom->documentElement->getAttribute("Mode");
+my $bpp;
+if ($mode eq "0") {
+	$bpp = 1;
+} elsif ($mode eq "1") {
+	$bpp = 2;
+} elsif ($mode eq "2") {
+	$bpp = 4;
+} elsif ($mode eq "4") {
+	$bpp = 1;
+} elsif ($mode eq "5") {
+	$bpp = 2;
+} else {
+	die "Bad mode attribute : $mode";
+}
+
+my $bpp_mas = (1 << $bpp)-1;
+my $ppb = int(8 / $bpp);
+
+print "BPP: $bpp : $bpp_mas : $ppb\n";
+
+foreach my $sprite ($dom->findnodes('/SpriteSheet/SpriteList/Sprite')) {
+    my $s_name = $sprite->getAttribute("Name");
+    my @s_bmp = unpack("C*", decode_base64($sprite->findvalue("Bitmap")));
+    my $s_w = $sprite->getAttribute("Width");
+    my $s_h = $sprite->getAttribute("Height");    
+    my $bytes_w = int(($s_w + $ppb - 1) / $ppb);
+    my $bytes_wm = int(($s_w + 8 - 1) / 8);
+    my $mask_w = int(($s_w + 7) / 8);
+    print "$s_name:\t$bytes_w x $s_h";
+    if ($mask) {
+    	print ", M:$mask_w x $s_h";
+    }
+    print "\n";
+    my @bin = (0) x $bytes_w * $s_h;
+    for (my $y = 0; $y < $s_h; $y++) {
+    	for (my $x = 0; $x < $s_w; $x++) {
+			my $c = pixelformat($s_bmp[$x + $y * $s_w], $bpp);
+			my $sh = $ppb - ($x % $ppb) - 1;
+			my $bx = int($x / $ppb);
+
+			$bin[$bx + ($y * $bytes_w)] |= ($c << $sh);
+    	}
+    }
+	print $fh_out pack("C*", @bin);
+
+	# append sprite mask
+	if ($mask) {
+	    my @binm = (0) x $bytes_wm * $s_h;
+	    for (my $y = 0; $y < $s_h; $y++) {
+	    	for (my $x = 0; $x < $s_w; $x++) {
+				my $c = ($s_bmp[$x + $y * $s_w] == 255)?0:1;
+				my $sh = 8 - ($x % 8) - 1;
+				my $bx = int($x / 8);
+
+				$binm[$bx + ($y * $bytes_wm)] |= ($c << $sh);
+	    	}
+	    }
+		print $fh_out pack("C*", @binm);
+	}    
+
+}
 
 
-sub Usage($) {
-	my ($msg) = @_;
+close $fh_out;
 
-	print STDERR "ERROR: $msg\n";
 
-print STDERR "Usage: beebspriter2blit.pl [..options..] <input> <output> [<symbols>]
+
+sub Usage($$$) {
+	my ($fh,$msg,$ex) = @_;
+
+	if ($msg) {
+		print $fh "ERROR: $msg\n";
+	}
+
+	print $fh "Usage: beebspriter2blit.pl [..options..] <input> <output> [<symbols>]
 
 Options:
 	--mask	Output masks after the 
 ";
 
-	exit 10;
+	exit $ex;
+}
+
+sub pixelformat($$) {
+	my ($c, $bpp) = @_;
+
+	if ($c == 255) {
+		return 0;
+	} elsif ($bpp == 1) {
+		return $c & 1;
+	} elsif ($bpp == 2) {
+		return ($c & 1) | (($c & 2) << 3);
+	} elsif ($bpp == 4) {
+		return ($c & 1) | (($c & 2) << 1) | (($c & 4) << 2) | (($c & 8) << 3);
+	}
+
 }
---
+++ src/examples/blitter/blit1/blit1.bas
@@ -0,0 +1,95 @@
+REM >1BPPFO3 A BASIC example of 1BPP blits with horizontal and vertical shifts
+
+*XMLOAD S.ISHSPR #D1 20000
+MODE 1:BPP%=2:SCRX%=320:SCRY%=256:COLOUR 129:CLS
+DIM TEMP% 4
+
+SCR_CHAR_ROW%=SCRX%*BPP%
+
+REM BLTCON is written in two passes with top bit clear i.e. not BLTCON_ACT_ACT
+REM the exec flags are first set
+REM then with top bit set the active flag, bit mode and cell flags are set
+REM BLTCON/ACT byte flags
+
+BLITCON_ACT_ACT=&80:REM always set when setting act constants/execing  
+BLITCON_ACT_CELL=&40:REM cell addressing used i.e. move one byte left adds 8 to address moving one line down either adds 1 byte or STRIDE depending on whether line crosses an 8 line boundary
+BLITCON_ACT_MODE_1BPP=&00:REM 1 bit per pixel mapping 2 colours
+BLITCON_ACT_MODE_2BPP=&10:REM 2 bit per pixel mapping 4 colours
+BLITCON_ACT_MODE_4BPP=&20:REM 4 bit per pixel mapping 16 colours
+BLITCON_ACT_MODE_8BPP=&30:REM 8 bit per pixel mapping 256 colours
+BLITCON_ACT_LINE=&08:REM draw a line
+BLITCON_ACT_COLLISION=&04:REM gets reset for any non-zero D data (even in EXEC_D is clear)
+BLITCON_ACT_WRAP=&02:REM wrap C/D addresses to fit between min/max
+BLITCON_EXEC_A=&01
+BLITCON_EXEC_B=&02
+BLITCON_EXEC_C=&04
+BLITCON_EXEC_D=&08
+BLITCON_EXEC_E=&10
+
+DMAC_BLITCON=&FD60
+DMAC_FUNCGEN=&FD61
+DMAC_WIDTH=&FD62
+DMAC_HEIGHT=&FD63
+DMAC_SHIFT=&FD64
+DMAC_MASK_FIRST=&FD65
+DMAC_MASK_LAST=&FD66
+DMAC_DATA_A=&FD67
+DMAC_ADDR_A=&FD68
+DMAC_DATA_B=&FD6B
+DMAC_ADDR_B=&FD6C
+DMAC_ADDR_C=&FD6F
+DMAC_ADDR_D=&FD72
+DMAC_ADDR_E=&FD75
+DMAC_STRIDE_A=&FD78
+DMAC_STRIDE_B=&FD7A
+DMAC_STRIDE_C=&FD7C
+DMAC_STRIDE_D=&FD7E
+
+
+PROCSELDMAC:
+
+
+Y%=0
+FOR A%=0TO2:FOR X%=16TO19
+PROCBlitSprite(A%,X%,Y%)
+Y%=Y%+14
+NEXT:NEXT
+
+END
+
+:
+DEFPROCBlitSprite(A%,X%,Y%):LOCAL CC%,SA%,SHX%,W%
+SA%=&FF3000+(X% DIV 4)*8+(Y% DIV 8)*640 + (Y% MOD 8)
+W%=4
+SHX%=X% MOD 4:IFSHX%<>0 THEN W%=W%+1:REM if there's a shift we need to widen the blit
+REM plot a non masked sprite data is read from channel B, channel C is used to read 
+REM existing screen data. The A channel is not executed (no mask) but the A channel
+REM must be initialized with &FF to not mask off data
+REM Sprite is placed on an arbitrary scan line within a cell using the CELL mode
+REM address generator
+
+?DMAC_BLITCON=BLITCON_EXEC_B+BLITCON_EXEC_C+BLITCON_EXEC_D: REM bitmap from B, original screen C, write screen D
+?DMAC_FUNCGEN=&CA:REM (A AND B) OR (NOT A AND C)
+?DMAC_WIDTH=W%-1
+?DMAC_HEIGHT=12-1
+?DMAC_SHIFT=SHX%*16
+REM as there may be a shift we need to mask off bits of the mask accordingly
+?DMAC_MASK_FIRST=FNshr(&FF,SHX%)
+IF SHX%=0 THEN ?DMAC_MASK_LAST=&FF ELSE ?DMAC_MASK_LAST=FNshr(&FF00,SHX%)
+?DMAC_DATA_A=&FF
+PROCPOKE24(DMAC_ADDR_B, &20000 + (A% MOD 3) * &48)
+PROCPOKE24(DMAC_ADDR_C, SA%)
+PROCPOKE24(DMAC_ADDR_D, SA%)
+PROCPOKE16(DMAC_STRIDE_B, 4)
+PROCPOKE16(DMAC_STRIDE_C, SCR_CHAR_ROW%)
+PROCPOKE16(DMAC_STRIDE_D, SCR_CHAR_ROW%)
+?DMAC_BLITCON=BLITCON_ACT_ACT+BLITCON_ACT_CELL+BLITCON_ACT_MODE_2BPP
+ENDPROC
+:
+DEFPROCSELDMAC:?&EE=&D1:?&FCFF=&D1:?&FCFE=&FC:?&FCFD=&FE:ENDPROC:REM Select JIM device and set page to chipset
+:
+DEFPROCPOKE24(A%,V%):!TEMP%=V%:A%?0=TEMP%?2:A%?1=TEMP%?1:A%?2=TEMP%?0:ENDPROC:REM Quick 24bit big-endian address poke
+:
+DEFPROCPOKE16(A%,V%):!TEMP%=V%:A%?0=TEMP%?1:A%?1=TEMP%?0:ENDPROC:REM Quick 16bit big-endian poke
+:
+DEFFNshr(V%,N%):LOCALI%:IFN%>0THENFORI%=1TON%:V%=V%DIV2:NEXT:=V%ELSE=V%
