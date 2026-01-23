701e8a1b7dc12355e2464a927c0e119efb176fed
dominicbeesley <8866586+dominicbeesley@users.noreply.github.com>
2026-01-23 18:08:40 +0000

    PREBOOT: set main title

--- src/roms/preboot/preboot2-c20k/main.c
+++ src/roms/preboot/preboot2-c20k/main.c
@@ -17,12 +17,50 @@ extern char main_head[];
 
 const char *w_main_data = "HELLO WORLD";
 
+rectangle r_head = {{3, 4}, {35, 3}};
+rectangle r_status = {{0, 24}, {40, 1}};
+rectangle r_main = {{0, 8}, {40, 16}};
 win_def w_main;
 win_def w_head;
 win_def w_status;
 lb_def l_list;
 
 char buf[100];
+
+
+const char *str_head_mainmenu = "\x02Main Menu";
+const char *str_head_pleaseselect = "\x06" "Cursor selects item, press return.";
+
+const char *head_title;
+const char *head_subtitle;
+bool render_head(win_def *w, void *arg) {
+	surface s;
+	point p;
+
+	debug_printf("RP\n");
+
+	p = point0;
+
+	surface_from_window(&s, w);
+
+	if (head_title) {
+		surface_render_str(&s, &p, head_title);
+		p.Y++;
+		surface_render_str(&s, &p, head_title);
+	}
+
+	if (head_subtitle) {
+		p.Y = 2;
+		surface_render_str(&s, &p, head_subtitle);
+	}
+	
+}
+
+void set_head_title(const char *title, const char *subtitle) {
+	head_title = title;
+	head_subtitle = subtitle;
+	win_refresh(&w_head);
+}
 
 
 bool render_status(win_def *w, void *arg) {
@@ -79,15 +117,14 @@ void l_list_render(win_def *w, lb_def *l, surface *s, int ix) {
 	char *p = buf;
 	unsigned long addr;
 	point point01 = {0, 1};
-
 	if (l->selected_index == ix) {
 		*p++ = 0x86;
 		*p++ = 0x9D;
 		*p++ = 0x83;
 	} else {
-		*p++ = '.';
-		*p++ = '.';
-		*p++ = '.';
+		*p++ = ' ';
+		*p++ = ' ';
+		*p++ = ' ';
 	}
 
 	addr = romset_find(ix);
@@ -113,11 +150,6 @@ void wait() {
 	__asm__("bne @lp");
 }
 
-extern unsigned char testval;
-surface surf_test;
-rectangle r_head = {{3, 4}, {25, 3}};
-rectangle r_status = {{0, 24}, {40, 1}};
-rectangle r_main = {{0, 8}, {40, 16}};
 
 int main(void) {
 
@@ -131,47 +163,31 @@ int main(void) {
 	hw_init();
 	keyb_init();
 
-	debug_printf("HELLO2\n");
-
 	memcpy((char *)0x7C00, main_head, 8*40);
 
 	screen_cursor_at(&point0);
 	screen_cursor(1);
 
-//TODO: why have to cast here function arguments should be copied so wtf?
-//	win_init(&w_head, 0, &r_head, NULL);
-//	win_open(&w_head, 0);
+	win_init(&w_head, 0, &r_head, NULL);
+	win_register_event(&w_head, EVENT_RENDER, &render_head);
+	win_open(&w_head, 0);
 
-	win_init(&w_status, 0, &r_status, NULL);
+	win_init(&w_status, WINDOW_OPT_NOCLEAR, &r_status, NULL);
 	win_register_event(&w_status, EVENT_RENDER, &render_status);
 	win_open(&w_status, 1);
 
 
 
-//	win_init(&w_main, WINDOW_OPT_NOCLEAR, &r_main, NULL);
-	//win_register_event(&w_main, EVENT_RENDER, &render_main);
-//	win_open(&w_main, 1);
+	win_init(&w_main, WINDOW_OPT_NOCLEAR, &r_main, NULL);
+//	win_register_event(&w_main, EVENT_RENDER, &render_main);
+	win_open(&w_main, 1);
 	
-debug_printf("HERE1\n");
 
-//	lb_init(&w_main, &l_list, &l_list_render, 5, 2);
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	wait();
-//	l_list.selected_index = 3;
-//	win_refresh(&w_main);
+	lb_init(&w_main, &l_list, &l_list_render, 2, 2);
+	l_list.selected_index = 0;
+	win_refresh(&w_main);
+
+	set_head_title(str_head_mainmenu, str_head_pleaseselect);
 
 	do { 
 
